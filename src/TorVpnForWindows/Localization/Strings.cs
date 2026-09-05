83c78ee337066f6f80ec5d1c62e8e79692b8060d
muhammetozeski <muhammetozeski@gmail.com>
2026-09-05 20:37:33 +0300

    Say IP verified rather than naming the service that checked it

--- src/TorVpnForWindows/Localization/Strings.cs
+++ src/TorVpnForWindows/Localization/Strings.cs
@@ -46,8 +46,8 @@ internal static class Strings
     public static string StatusExitCountry = "Çıkış ülkesi";
     public static string StatusChecking = "Denetleniyor…";
     public static string StatusUnknown = "Bilinmiyor";
-    public static string StatusNotConfirmed = "Tor Project bu bağlantıyı doğrulayamadı.";
-    public static string StatusConfirmed = "Tor Project tarafından doğrulandı";
+    public static string StatusNotConfirmed = "IP doğrulanamadı, bu bir Tor çıkış rölesi değil";
+    public static string StatusConfirmed = "IP doğrulandı";
     public static string StatusDownload = "Alınan";
     public static string StatusUpload = "Gönderilen";
     public static string StatusUdpNotice = "Tor yalnızca TCP taşır. UDP reddedilir, uygulamalar TCP'ye döner.";
